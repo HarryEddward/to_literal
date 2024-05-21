8e0b1ff35359e58d16c9093d5fd140e3d25b35a2
HarryEddward <he0780070@gmail.com>
2024-05-21 22:39:47 +0200

    changes

--- src/setup.py
+++ src/setup.py
@@ -4,6 +4,10 @@ setup(
     name='to_literal',
     version='v1',
     description='Librery to use it in a simple way to do a cast of an array to Literal[], to use it for validation with Pydantic',
+    long_description=
+"""
+# Hola que tal?
+""",
     author='Adrià Martín Martorell',
     author_email='au7812ooae32@gmail.com',
     maintainer='Adrià Martín Martorell',
